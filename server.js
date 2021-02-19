91226561b66dcf8bdd66dc16de8e0164522d141d
tinacohen1 <78365834+tinacohen1@users.noreply.github.com>
2021-02-19 00:02:08 -0500

    rev 3

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const morgan = require('morgan')
 
 const app = express()
 
-
 app.use(morgan('dev'))
 
 
@@ -12,20 +11,34 @@ console.log(process.env.API_TOKEN)
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
+// -------- validation middleware
+app.use(function validateBearerToken(req, res, next) {
+  console.log('validate bearer token middleware')
+  // move to the next middleware
+  next()
+})
+
+// -------- end point /types
 function handleGetTypes(req, res) {
   res.json(validTypes)
 }
-
 app.get('/types', handleGetTypes)
 
-const PORT = 8000
+// -------- end point /pokemon
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!')
+}
+app.get('/pokemon', handleGetPokemon)
 
+
+const PORT = 8000
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
 
 /*
 endpints
-http://localhost:8000/
+http://localhost:8000/types
+http://localhost:8000/pokemon
 */
 
