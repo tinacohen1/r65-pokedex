0d01ae501cc59377dd8fe5a27111cd46ea987fe6
tinacohen1 <78365834+tinacohen1@users.noreply.github.com>
2021-02-18 23:00:51 -0500

    rev -2 env rworks

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
 const app = express()
+
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
-})
+
+console.log(process.env.API_TOKEN)
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+  res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
 
 const PORT = 8000
 
@@ -14,5 +24,8 @@ app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
 
-
+/*
+endpints
+http://localhost:8000/
+*/
 
